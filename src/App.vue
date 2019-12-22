5a25f9283179d7b710e944d53792bc4eabe4a612
echrs <echrs@outlook.com>
2019-12-22 17:43:57 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col p-0">
+          <nav class="navbar navbar-expand-lg p-0" style="background-color: #A0E0F6">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">
+                    <router-link to="/"><span class="mif-home mif-2x fg-white"></span></router-link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <router-link to="/"><span class="mif-mail mif-2x fg-white"></span></router-link>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    <router-link to="/"><span class="mif-share mif-2x fg-white"></span></router-link>
+                  </a>
+                </li>
+              </ul>
+            <div id="namebranda" class="navbar-brand d-none d-lg-block" href="#">
+              <router-link to="/"><img src="/images/logo.png" width="70" height="40" alt="Logo fibot" /></router-link>
+            </div>
+          </nav>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="container-fluid h-100">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -19,7 +45,6 @@
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-6">
+      1 of 2
+    </div>
+    <div class="col-6" style="background-color: #EFFAFE">
+      2 of 2
+    </div>
+  </div>
   </div>
 </template>
 
