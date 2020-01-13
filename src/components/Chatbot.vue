265e1ac728fd3542e7fb93765839eacf7ccd91b8
echrs <echrs@outlook.com>
2020-01-13 19:45:53 +0100

    component added

--- src/components/Chatbot.vue
+++ src/components/Chatbot.vue
@@ -2,14 +2,30 @@
   <v-flex md6>
     <v-card color="#EFFAFE" flat height="100%">
       <v-card-text class="text-center">
-        <div id="webchat"/>
+    <Widget
+    :passedProps="passedProps"
+    />
       </v-card-text>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+import { ReactWrapper } from 'vuera'
+import { Widget } from 'rasa-webchat';
 export default {
-  name: 'Chatbot'
+  computed: {
+    passedProps () {
+      return {
+      initPayload: '/get_started',
+      socketUrl:'http://localhost:5005',
+      socketPath:'/socket.io/',
+      customData:'"language": "hr"',
+      title:'Fi'
+      }
+    }
+  },
+  name: 'Chatbot',
+  components: { 'Widget': Widget },
 }
 </script>
